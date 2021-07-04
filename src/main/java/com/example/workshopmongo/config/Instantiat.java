9e18f02903e2444653edbf377fbbb4c0e1ef88ab
Erick Oliveira <erick.oliveira9607@gmail.com>
2021-07-04 13:05:02 -0300

    Adicionando comentários aos posts

--- src/main/java/com/example/workshopmongo/config/Instantiat.java
+++ src/main/java/com/example/workshopmongo/config/Instantiat.java
@@ -11,6 +11,8 @@ import org.springframework.context.annotation.Configuration;
 import com.example.workshopmongo.domain.Post;
 import com.example.workshopmongo.domain.User;
 import com.example.workshopmongo.dto.AuthorDTO;
+import com.example.workshopmongo.dto.CommentDTO;
+import com.example.workshopmongo.dto.UserDTO;
 import com.example.workshopmongo.repository.PostRepository;
 import com.example.workshopmongo.repository.UserRepository;
 
@@ -38,10 +40,16 @@ public class Instantiat implements CommandLineRunner {
 
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem!", "Vou viajar para o Paraná",
-				new AuthorDTO(maria));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem!", "Vou viajar para o Paraná", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje", new AuthorDTO(maria));
 
+		CommentDTO c1 = new CommentDTO("Boa Viagem Mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post1.getComments().add(c3);
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
 		maria.getPosts().add(post1);
